cad81d85d80b11bbe3e2e87a724aa84bef9d87eb
kode2go <brsbin001@myuct.ac.za>
2023-01-20 06:05:15 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import openai
+
+openai.api_key = "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"
+
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt='What is an angiogram?',
+    temperature=0.5
+)
+
+answer = response["choices"][0]["text"]
+# st.write(answer)
+print(answer)
