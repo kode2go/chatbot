595a839dfe1f49d241caf234c1ced2c0e5d42dea
kode2go <brsbin001@myuct.ac.za>
2023-01-20 06:17:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import streamlit as st
 
 openai.api_key = "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"
 
+"""
+# Welcome to My Hospital Chatbot!
+
+Feel free to ask any questions below:
+
+"""
+
+chatbot_input = st.text_input('What is an angiogram?')
+
 response = openai.Completion.create(
     engine="text-davinci-002",
-    prompt='What is an angiogram?',
+    prompt=chatbot_input,
     temperature=0.5
 )
 
