e3e90d57ef3bc1a9d7afa361cee1c98c19d0f101
kode2go <brsbin001@myuct.ac.za>
2023-01-21 07:37:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ from gsheetsdb import connect
 # Create a connection object.
 conn = connect()
 sheet_url = st.secrets["public_gsheets_url"]
-#rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 """
 # Welcome to My Hospital Chatbot!
